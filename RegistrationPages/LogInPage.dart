b0eb121b26179a0f8e86482a2178b1b2b81e83fb
khaled alabdullh <khaledabdullah2001104@gmail.com>
2022-09-24 17:21:45 +0300

    create signin page and use Email_Password_page

--- RegistrationPages/LogInPage.dart
+++ RegistrationPages/LogInPage.dart
@@ -1,3 +1,4 @@
+import 'package:exam_app/RegistrationPages/Email_Password_Page.dart';
 import 'package:flutter/material.dart';
 
 class LogInPage extends StatelessWidget {
@@ -24,57 +25,9 @@ class LogInPage extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextFormField(
-                    controller: login_controller_email,
-                    keyboardType: TextInputType.emailAddress,
-                    autofillHints: [AutofillHints.email],
-                    validator: ((value) {
-                      if (value!.isEmpty || value == null) {
-                        return "empty Email";
-                      } else {
-                        bool state = RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')
-                            .hasMatch(value);
-                        if (!state) {
-                          return "this is not email";
-                        }
-                      }
-                    }),
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      prefixIcon: Icon(Icons.email),
-                    ),
-                    style: TextStyle(
-                      fontFamily: "",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    controller: login_controller_password,
-                    keyboardType: TextInputType.visiblePassword,
-                    autofillHints: [AutofillHints.password],
-                    validator: ((value) {
-                      if (value!.isEmpty || value == null) {
-                        return "Password must be eight letters and contain numbers and a large character";
-                      } else {
-                        bool state =
-                            RegExp("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}")
-                                .hasMatch(value);
-                        if (!state) {
-                          return "this password is not currect";
-                        }
-                      }
-                    }),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      prefixIcon: Icon(Icons.password),
-                    ),
-                    style: TextStyle(
-                      fontFamily: "",
-                    ),
-                  ),
+                  // I use the class who contanes at text form field to email and password
+                  Email_Password(
+                      login_controller_email, login_controller_password),
                   SizedBox(
                     height: 20,
                   ),
